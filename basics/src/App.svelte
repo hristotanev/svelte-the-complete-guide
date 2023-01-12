466967dbd62906b973ebff7886246536c36e67c8
hristotanev <hristotanev7@gmail.com>
2023-01-12 17:27:58 +0000

    feat: Basics assignment solution

---
+++ basics/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Goal from "./lib/Goal.svelte";
+
+  let goal;
+</script>
+
+<main>
+  <input bind:value={goal}/>
+  <Goal {goal} />
+</main>
---
+++ basics/src/lib/Goal.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let goal;
+</script>
+
+<style>
+  .goal {
+    color: red;
+  }
+</style>
+
+<h1 class="goal" class:goal={(/\!+/).test(goal)}>{goal}</h1>
